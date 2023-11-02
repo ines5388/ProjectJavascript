6f6bacfec13da63d15f5fc784480f1b411065a19
ines5388 <142507801+ines5388@users.noreply.github.com>
2023-11-02 12:49:46 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = laExcusa();
+  document.querySelector("#boton").addEventListener("click", function() {
+    document.querySelector("#excusa").innerHTML = laExcusa();
+  });
+};
+
+let laExcusa = () => {
+  let tipoDePasta = ["Gnoquis", "Sorrentinos", "Canelones"];
+  let tipoDeSalsa = [
+    "bolognesa",
+    "carbonara",
+    "rosa",
+    "de crema y champignon",
+    "mixta"
+  ];
+  let quien = ["mi mamá", "mi abuela", "mi hermano", "mi sobrina"];
+  let tipoDePastaIndice = Math.floor(Math.random() * tipoDePasta.length);
+  let tipoDeSalsaIndice = Math.floor(Math.random() * tipoDeSalsa.length);
+  let quienIndice = Math.floor(Math.random() * quien.length);
+
+  return (
+    tipoDePasta[tipoDePastaIndice] +
+    " con salsa " +
+    tipoDeSalsa[tipoDeSalsaIndice] +
+    " hecha por " +
+    quien[quienIndice]
+  );
 };
